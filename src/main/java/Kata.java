1430289c77c08361f7a99999cbed2cf36ddf5eed
YannMancel <yann.mancel@hotmail.fr>
2025-08-05 19:34:46 +0200

    feat: step 1 - update Kata.fizzBuzz to take into account FizzBuzz if divisible by 3 and 5

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,10 +1,12 @@
 public class Kata {
     public static Object fizzBuzz(int value) {
-        return isFizz(value)
-                ? "Fizz"
-                : isBuzz(value)
-                    ? "Buzz"
-                    : value;
+        return isFizz(value) && isBuzz(value)
+                ? "FizzBuzz"
+                : isFizz(value)
+                    ? "Fizz"
+                    : isBuzz(value)
+                        ? "Buzz"
+                        : value;
     }
 
     public static boolean isFizz(int value) {

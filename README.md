5a10fe3e6d62cdef6ebaf0f5c09fbaf90bc06e07
YannMancel <yann.mancel@hotmail.fr>
2025-08-05 10:05:03 +0200

    chore: initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+![badge_java]
+
+# FizzBuzz
+**Goal**: A Java project to study the [FizzBuzz Kata](https://codingdojo.org/fr/kata/FizzBuzz/).
+
+[badge_java]: https://img.shields.io/badge/Java-21-blue
---
+++ src/test/java/fizz_buzz_test.java
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FizzBuzzTest {
+    @Test
+    void should_display_number() {
+        fail();
+    }
+}

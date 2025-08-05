e6af3733684c5cbe6412637a8bf1c5cb3900ea02
YannMancel <yann.mancel@hotmail.fr>
2025-08-05 19:12:48 +0200

    test: step 1 - display Fizz if divisible by 3

--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -12,12 +12,13 @@ class KataTest {
         return IntStream.iterate(1, (previous) -> previous + 1).limit(100);
     }
 
-    @DisplayName("Should display number between [1 , 100]")
-    @ParameterizedTest(name = "{index} -> Number is {0}")
+    @DisplayName("For a number between [1 , 100]")
+    @ParameterizedTest(name = "{index} -> Should display correct output")
     @MethodSource("iterateFrom1To100")
     void should_display_number(int value) {
         assertThat(value)
                 .isBetween(1, 100)
+                .extracting((e) -> e % 3 == 0 ? "Fizz" : e)
                 .isEqualTo(Kata.fizzBuzz(value));
     }
 }

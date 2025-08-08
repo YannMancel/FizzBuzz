7b038fd811554013746161689f3773c7ce964d5a
YannMancel <yann.mancel@hotmail.fr>
2025-08-08 08:15:47 +0200

    test: step 3 - display the joint of all iterations

--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -11,28 +13,55 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class KataTest {
 
+    static IntStream iterateFrom1To100() {
+        return IntStream.rangeClosed(1, 100);
+    }
+
     static Stream<Arguments> iterateArgumentsFrom1To100() {
-        return IntStream
-            .rangeClosed(1, 100)
-            .mapToObj((e) -> arguments(e, Kata.fizzBuzz(e)));
+        return iterateFrom1To100().mapToObj((e) -> arguments(e, Kata.fizzBuzz(e)));
+    }
+
+    Object getExpectedOutput(int input) {
+        return Kata.isDivisibleBy3(input) && Kata.isDivisibleBy5(input)
+            ? "FizzBuzz"
+            : Kata.isDivisibleBy3(input) || Kata.contains3(input)
+                ? "Fizz"
+                : Kata.isDivisibleBy5(input) || Kata.contains5(input)
+                    ? "Buzz"
+                    : input;
     }
 
     @DisplayName(
-        "When input is a number between [1 , 100], " +
-        "Should display correct output")
+        "Given input is a number between [1 , 100], " +
+        "When fizzbuzz is called, " +
+        "Then displays correct output")
     @ParameterizedTest(name = "{index} -> {1}")
     @MethodSource("iterateArgumentsFrom1To100")
-    void should_display_number(int value, Object output) {
-        assertThat(value)
+    void should_display_correct_output_when_fizzbuzz_is_called(int input, Object output) {
+        assertThat(input)
             .isBetween(1, 100)
-            .extracting((e) -> Kata.isDivisibleBy3(e) && Kata.isDivisibleBy5(e)
-                ? "FizzBuzz"
-                : Kata.isDivisibleBy3(e) || Kata.contains3(e)
-                    ? "Fizz"
-                    : Kata.isDivisibleBy5(e) || Kata.contains5(e)
-                        ? "Buzz"
-                        : e
-            )
+            .extracting(this::getExpectedOutput)
             .isEqualTo(output);
+    }
+
+    @DisplayName(
+        "Given inputs are numbers between [1 , 100], " +
+        "When compute is called, " +
+        "Then displays correct output")
+    @Test
+    void should_display_correct_output_when_compute_is_called() {
+        final List<Integer> inputs = iterateFrom1To100()
+            .boxed()
+            .toList();
+
+        final StringBuilder buffer = new StringBuilder(inputs.size());
+        inputs.forEach((input) -> {
+            final String line = "input: " + input + " -> output: " + getExpectedOutput(input) + "\n";
+            buffer.append(line);
+        });
+
+        assertThat(buffer.toString())
+            .isNotEmpty()
+            .isEqualTo("foo");
     }
 }
